43ff83b6287841fd57a05e37286071cf011e6e0c
Joel Flores <j_flores88@outlook.com>
2021-07-19 19:45:30 -0700

    Create factory that creates new projects

---
+++ src/projectFactory.js
@@ -0,0 +1,9 @@
+function projectFactory(projectName) {
+    this.name = projectName;
+    
+    const newProject = document.createElement('ul');
+  
+    // appendToPage(name, newProject);
+};
+
+export { projectFactory };
