f96d4d533282adb7e3cc15baf77a6714e1a127ae
Joel Flores <j_flores88@outlook.com>
2021-08-04 17:21:22 -0700

    Changed styles.css link

--- webpack.config.js
+++ webpack.config.js
@@ -13,28 +13,28 @@ module.exports = {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
-        {
-          test: /\.(scss)$/,
-          use: [{
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader'
-          }, {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: function () {
-                  return [
-                    require('precss'),
-                    require('autoprefixer')
-                  ];
-                }
-              }
-            }
-          }, {
-            loader: 'sass-loader'
-          }]
-        }
+        // {
+        //   test: /\.(scss)$/,
+        //   use: [{
+        //     loader: 'style-loader'
+        //   }, {
+        //     loader: 'css-loader'
+        //   }, {
+        //     loader: 'postcss-loader',
+        //     options: {
+        //       postcssOptions: {
+        //         plugins: function () {
+        //           return [
+        //             require('precss'),
+        //             require('autoprefixer')
+        //           ];
+        //         }
+        //       }
+        //     }
+        //   }, {
+        //     loader: 'sass-loader'
+        //   }]
+        // }
       ]
     }
 };
